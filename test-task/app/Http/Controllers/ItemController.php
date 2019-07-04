ff9fec22618c1f877e182ab506c70cd18985b07f
a2881dante <a2881dante.dev@gmail.com>
2019-07-04 03:39:38 +0300

    Create controllers for Item and Size.

---
+++ test-task/app/Http/Controllers/ItemController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Item;
+use Illuminate\Http\Request;
+
+class ItemController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Item::with('sizes')->get();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $item = new Item([
+            'name'          => $request->query('item-name'),
+            'avatar_url'    => ''
+        ]);
+        $item->save();
+        foreach ($request->query('sizes') as $size){
+            $item->sizes()->attach($size);
+        }
+        return $item;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return Item::with('sizes')->find($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $item = Item::find($id);
+        $item->name = $request->query('name');
+        $item->save();
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $item = Item::find($id);
+        $item->sizes()->detach();
+        $item->delete();
+    }
+}
